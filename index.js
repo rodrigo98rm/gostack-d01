9f06488766b9e4aaf5f09905c126a0788d0d7369
rodrigo98rm <mayerrodrigo98@gmail.com>
2019-07-02 20:38:03 -0300

    Desafio concluido

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
+//Descrição
+//https://github.com/Rocketseat/bootcamp-gostack-desafio-01/blob/master/README.md
+
 const express = require("express");
 const server = express();
 
 server.use(express.json());
 
+//Log number of requests
+server.use((req, res, next) => {
+  count++;
+  console.log(`${count} ${req.method} - ${req.url}`);
+  return next();
+});
+
 let projects = [];
+let count = 0;
 
 //GET PROJECTS
 server.get("/projects", (req, res) => {
   return res.json(projects);
 });
-
-function checkNewProjData(req, res, next) {
-  const { id, title } = req.body;
-  if (!id) {
-    return res.status(400).json({ error: "Missing attribute: id" });
-  }
-  if (!title) {
-    return res.status(400).json({ error: "Missing attribute: title" });
-  }
-  return next();
-}
 
 //CREATE PROJECT
 server.post("/projects", checkNewProjData, (req, res) => {
@@ -34,14 +34,60 @@ server.post("/projects", checkNewProjData, (req, res) => {
 });
 
 //UPDATE PROJECT
-server.put("/projects/:id", (req, res) => {
-  const { id } = req.params;
+server.put("/projects/:id", checkProjExists, (req, res) => {
+  // const { id } = req.params;
   const { title } = req.body;
 
-  let proj = projects.find(proj => proj.id == id);
-  proj.title = title;
+  // let proj = projects.find(proj => proj.id == id);
+  req.proj.title = title;
 
-  return res.json(proj);
+  return res.json(req.proj);
 });
+
+//DELETE PROJECT
+server.delete("/projects/:id", checkProjExists, (req, res) => {
+  // const { id } = req.params;
+
+  // let proj = projects.find(proj => proj.id == id);
+  const index = projects.indexOf(req.proj);
+
+  projects.splice(index, 1);
+
+  return res.json();
+});
+
+//ADD TASK TO A PROJECT
+server.post("/projects/:id/tasks", checkProjExists, (req, res) => {
+  // const { id } = req.params;
+  const { title } = req.body;
+
+  // let proj = projects.find(proj => proj.id == id);
+  req.proj.tasks.push(title);
+
+  return res.json(req.proj);
+});
+
+//MIDDLEWARES
+function checkNewProjData(req, res, next) {
+  const { id, title } = req.body;
+  if (!id) {
+    return res.status(400).json({ error: "Missing attribute: id" });
+  }
+  if (!title) {
+    return res.status(400).json({ error: "Missing attribute: title" });
+  }
+  return next();
+}
+
+function checkProjExists(req, res, next) {
+  const { id } = req.params;
+
+  let proj = projects.find(proj => proj.id == id);
+  if (!proj) {
+    return res.status(404).json({ error: `Project with id ${id} not found` });
+  }
+  req.proj = proj;
+  return next();
+}
 
 server.listen(3000);
